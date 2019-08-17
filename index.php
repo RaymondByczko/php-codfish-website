8848922a8bf26b3eb15c84117af6fb16c1135191
Raymond Byczko <raymondbyczko@att.net>
2019-08-17 14:01:50 -0400

    RByczko;2019-08-17;Added css styles.

--- index.php
+++ index.php
@@ -5,8 +5,30 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="author" content="Raymond Byczko">
+	<link rel="stylesheet" href="./css/styles.css">
+	<title>PHP Codfish Website | Welcome</title>
 </head>
 <body>
+	<header>
+		<div class="container">
+	        <div id="branding">
+	        	<h1><span class="highlight">PHP</span> Codfish</h1>
+	        </div>
+	        <nav>
+	          <ul>
+	            <li class="current"><a href="index.php">HOME</a></li>
+	            <li><a href="help.php">HELP</a></li>
+	            <li><a href="github.php">GITHUB</a></li>
+	          </ul>
+	        </nav>
+	    </div>
+	</header>
+<p>
+<section>
+<div class="container2">
 <div id="form_maken">
 <form action="/TitleUtilities/createTitleDataFile.php" method="post">
 Number Lines:<br>
@@ -27,5 +49,7 @@ if (isset($_SESSION['makeN.index']))
 }	
 ?>
 </div>
+</div>
+</section>
 </body>
 </html>
